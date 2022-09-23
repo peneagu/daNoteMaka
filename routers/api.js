be8e38abeb64933bf2a1ea80eea47eac005666e7
Precious <preciouseneagu8@icloud.com>
2022-09-23 00:53:08 -0700

    i think

--- routers/api.js
+++ routers/api.js
@@ -1,45 +1,41 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 let uniqid = require('uniqid');
 
 module.exports = (app) => {
-    //get all notes
     app.get('/api/notes', (req, res) => {        
         res.sendFile(path.join(__dirname, '../db/db.json'));
     });
 
-    //Get single note
-    app.get('/api/notes/:id', (req, res) => {
-      if (req.param.note_id) {
-        const noteId = req.params.note_id;
-        for(let i = 0; i < notes.length; i++) {
-            const currentNote = notes[i];   
-            if (currentNote.note_id === noteId) {
-                res.json(currentNote);
-                return;
-            }
-        }    
-      }
-    }
-    );
-    //Create new note    
     app.post('/api/notes', (req, res) => {
-        console.log('setting up new note');
+        console.log('set up new note');
         let db = fs.readFileSync('db/db.json');
         db = JSON.parse(db);
 
         let newNote = {
-            title: req.body.title,  
             text: req.body.text,
+            title: req.body.title,  
             note_id: uniqid()
         };
-        //add new note to db
         db.push(newNote);
         fs.writeFileSync('db/db.json', JSON.stringify(db));
         res.send(db);
     });
-    //Delete note
+
+    app.get('/api/notes/:id', (req, res) => {
+        if (req.param.note_id) {
+          const noteId = req.params.note_id;
+          for(let i = 0; i < notes.length; i++) {
+              const currentNotes = notes[i];   
+              if (currentNotes.note_id === noteId) {
+                  res.json(currentNotes);
+                  return;
+              }
+          }    
+        }
+    });
+ 
     app.delete('/api/notes/:id', (req, res) => {
         let db = JSON.parse(fs.readFileSync('db/db.json'));
         let deleteNotes = db.filter(item => item.id !== req.params.note_id);
--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require('express');
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
-// Middleware.
-app.use(express.static('public'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes.
 
 require('./routers/api')(app);
 require('./routers/html')(app);
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
